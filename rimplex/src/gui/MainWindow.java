9f78064bfcf1966e37d0daac675596fb53c848dd
Dylan Moreno <morenodd@dukes.jmu.edu>
2021-03-22 16:04:40 -0400

    added comment

--- rimplex/src/gui/MainWindow.java
+++ rimplex/src/gui/MainWindow.java
@@ -6,6 +6,9 @@ public class MainWindow extends JFrame
 {
   private static final long serialVersionUID = 2740437090361841747L;
 
+  /**
+   * Default Constructor.
+   */
   public MainWindow()
   {
     super();
