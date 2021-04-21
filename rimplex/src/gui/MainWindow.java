f91d5429db49c2e92f0b05471198dfbf47b2adb6
Max <bergermj@dukes.jmu.edu>
2021-04-20 20:38:00 -0400

    Refactored code for file saving.
    Moved code to a separate class.

--- rimplex/src/gui/MainWindow.java
+++ rimplex/src/gui/MainWindow.java
@@ -584,31 +584,7 @@ public class MainWindow extends JFrame implements ActionListener, KeyListener
     
     // Open save dialog
     if (e.getActionCommand().equals("Save Recorded Calculations")) {
-      JFileChooser fileChooser = new JFileChooser();
-      fileChooser.setDialogTitle("Specify a location to save calculations");   
-
-      int userSelection = fileChooser.showSaveDialog(null);
-       
-      File theFile = new File("");
-      PrintWriter outFile;
-      try
-      {
-        outFile = new PrintWriter(theFile);
-        outFile.print("asdf");
-        outFile.close();
-      }
-      catch (FileNotFoundException e1)
-      {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-      }
-      
-      fileChooser.setSelectedFile(theFile);
-      
-      if (userSelection == JFileChooser.APPROVE_OPTION) {
-          File fileToSave = fileChooser.getSelectedFile();
-          System.out.println("Save as file: " + fileToSave.getAbsolutePath());
-      }
+       FileDialog.saveCalcs();
     }
     
     // Open help webpage in default browser
