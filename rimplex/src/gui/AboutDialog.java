aa4aae17e762b8240fdfde611ca6fa3a346e5d1c
Kory Erdmann <erdmankp@dukes.jmu.edu>
2021-04-25 02:44:43 -0400

    grammar

--- rimplex/src/gui/AboutDialog.java
+++ rimplex/src/gui/AboutDialog.java
@@ -35,22 +35,22 @@ public class AboutDialog extends JOptionPane
   public void display()
   {
     if (language.equals("English")) {
-    showMessageDialog(null, "rimpleX is a desktop calculator designed to work with these numbers:\n\n• Real\n• Imaginary\n• Complex\n\nDeveloped by SagaciousMedia.", "About rimpleX", JOptionPane.PLAIN_MESSAGE);
+    showMessageDialog(null, "RimpleX is a desktop calculator designed to work with these numbers:\n\n• Real\n• Imaginary\n• Complex\n\nDeveloped by SagaciousMedia.", "About rimpleX", JOptionPane.PLAIN_MESSAGE);
     }
     if (language.equals("Spanish")) {
-    showMessageDialog(null,"rimpleX es una calculadora de escritorio diseñada para trabajar con estos números: \n \n • Real \n • Imaginario \n • Complejo \n \nDesarrollado por SagaciousMedia.", "Acerca de rimpleX", JOptionPane.PLAIN_MESSAGE);
+    showMessageDialog(null,"RimpleX es una calculadora de escritorio diseñada para trabajar con estos números: \n \n • Real \n • Imaginario \n • Complejo \n \nDesarrollado por SagaciousMedia.", "Acerca de rimpleX", JOptionPane.PLAIN_MESSAGE);
     }
     if (language.equals("French")) {
-    showMessageDialog(null,"rimpleX est une calculatrice de bureau conçue pour fonctionner avec ces nombres: \n \n • Réel \n • Imaginaire \n • Complexe \n \n Développé par SagaciousMedia.", "À propos de rimpleX", JOptionPane.PLAIN_MESSAGE);
+    showMessageDialog(null,"RimpleX est une calculatrice de bureau conçue pour fonctionner avec ces nombres: \n \n • Réel \n • Imaginaire \n • Complexe \n \n Développé par SagaciousMedia.", "À propos de rimpleX", JOptionPane.PLAIN_MESSAGE);
     }
     if (language.equals("Portugese")) {
-    showMessageDialog(null,"rimpleX é uma calculadora de desktop projetada para trabalhar com estes números: \n \n • Real \n • Imaginário \n • Complexo \n \n Desenvolvido por SagaciousMedia.", "Sobre rimpleX", JOptionPane.PLAIN_MESSAGE);
+    showMessageDialog(null,"RimpleX é uma calculadora de desktop projetada para trabalhar com estes números: \n \n • Real \n • Imaginário \n • Complexo \n \n Desenvolvido por SagaciousMedia.", "Sobre rimpleX", JOptionPane.PLAIN_MESSAGE);
     }
     if (language.equals("Japanese")) {
-    showMessageDialog(null,"rimpleXは次の数値で動作するように設計されたデスクトップ計算機です：\n \n•実数\n•虚数\n•複素数\n \nSagaciousMediaによって開発されました。", "rimpleXについて", JOptionPane.PLAIN_MESSAGE);
+    showMessageDialog(null,"RimpleXは次の数値で動作するように設計されたデスクトップ計算機です：\n \n•実数\n•虚数\n•複素数\n \nSagaciousMediaによって開発されました。", "rimpleXについて", JOptionPane.PLAIN_MESSAGE);
     }
     if (language.equals("Russian")) {
-    showMessageDialog(null,"rimpleX - настольный калькулятор, предназначенный для работы со следующими числами: \n \n • Реальные \n • Мнимые \n • Сложные \n \n Разработано SagaciousMedia.", "О rimpleX", JOptionPane.PLAIN_MESSAGE);
+    showMessageDialog(null,"RimpleX - настольный калькулятор, предназначенный для работы со следующими числами: \n \n • Реальные \n • Мнимые \n • Сложные \n \n Разработано SagaciousMedia.", "О rimpleX", JOptionPane.PLAIN_MESSAGE);
     }
   }
   
